96a8572eb535a7aac7d2d821dfa1de430188c04f
Alexey Volodin <alexeyw@gmail.com>
2025-06-11 21:23:47 -0400

    Configure repositories and JDK auto-download

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,15 @@ include(":app")
 pluginManagement {
     repositories {
         google()
+        mavenCentral()
         gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
     }
 }
